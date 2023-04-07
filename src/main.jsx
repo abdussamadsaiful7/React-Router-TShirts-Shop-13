1e75e31524f03678955b7d1aa7c9b0932193c1fa
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-04-07 17:03:09 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Components/Home/Home';
+import Root from './Components/Layout/root';
+import Orders from './Components/OrderReview/OrderReview';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:<Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+        loader: () =>fetch('shirt.json')
+      },
+      {
+        path: 'orders',
+        element: <Orders></Orders>
+      },
+      {
+        
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
